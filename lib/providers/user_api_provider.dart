5b6af7b71a6964190faf85d0082035d469ecc24f
Rajesh Menon <rajeshkuttiyilm@gmail.com>
2022-02-20 14:27:00 +0530

    api provider for cloud service login

---
+++ lib/providers/user_api_provider.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:bnotes/constants.dart';
+import 'package:nextcloud/nextcloud.dart';
+import 'package:http/http.dart' as http;
+
+class UserApiProvider {
+  static Future<User?> checkUserCredential(Map post) async {
+    String result = "";
+    try {
+      var loginResponse = await http.Client()
+          .post(Uri.parse(kBaseUrl + "services/checkUser"), body: post);
+      result = loginResponse.body;
+      print(result);
+      if (result.contains('{')) {
+        Map<String, dynamic> userMap = jsonDecode(result);
+        var user = new User.fromJson(userMap);
+        return user;
+      } else {
+        print(result);
+        return null;
+      }
+    } catch (ex) {
+      result = "Unable to check!\n" + ex.toString();
+      return null;
+    }
+  }
+}
