ac344e128c3d70f54eb96591f3036a4298958d23
Rajesh Menon <rajeshmenon@rennovationsoftware.com>
2023-09-17 19:39:36 +0530

    settings page layout

--- lib/mobile/pages/mobile_settings_page.dart
+++ lib/mobile/pages/mobile_settings_page.dart
@@ -1,5 +1,7 @@
 // import 'package:bnotes/helpers/globals.dart' as globals;
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:bnotes/helpers/globals.dart' as globals;
 
 class MobileSettingsPage extends StatefulWidget {
   const MobileSettingsPage({Key? key}) : super(key: key);
@@ -11,12 +13,42 @@ class MobileSettingsPage extends StatefulWidget {
 class _MobileSettingsPageState extends State<MobileSettingsPage> {
   @override
   Widget build(BuildContext context) {
-    // var brightness = MediaQuery.of(context).platformBrightness;
-    // bool darkModeOn = (globals.themeMode == ThemeMode.dark ||
-    //     (brightness == Brightness.dark &&
-    //         globals.themeMode == ThemeMode.system));
-    return const Scaffold(
-      body: FlutterLogo(),
+    return Scaffold(
+      body: ListView(
+        children: [
+          ListTile(
+            onTap: () {},
+            leading: const Icon(CupertinoIcons.person),
+            title: const Text('Account'),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: const Icon(CupertinoIcons.shield),
+            title: const Text('Security'),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: const Icon(CupertinoIcons.cloud),
+            title: const Text('Backup'),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: const Icon(CupertinoIcons.brightness),
+            title: const Text('Appearance'),
+            subtitle: Text(globals.themeMode.name),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: const Icon(CupertinoIcons.bell),
+            title: const Text('Notifications'),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: const Icon(CupertinoIcons.info),
+            title: const Text('About'),
+          ),
+        ],
+      ),
     );
   }
 }
