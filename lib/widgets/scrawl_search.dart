2eaf291582479ae2f2c2156359a0f3edf0e67b24
Rajesh Menon <rajeshmenon@rennovationsoftware.com>
2023-09-19 22:15:16 +0530

    search textfield widget

---
+++ lib/widgets/scrawl_search.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class ScrawlSearch extends StatefulWidget {
+  TextEditingController? controller;
+  final VoidCallback onSearch;
+  final VoidCallback onClearSearch;
+  ScrawlSearch(
+      {Key? key,
+      this.controller,
+      required this.onSearch,
+      required this.onClearSearch})
+      : super(key: key);
+
+  @override
+  State<ScrawlSearch> createState() => _ScrawlSearchState();
+}
+
+class _ScrawlSearchState extends State<ScrawlSearch> {
+  bool hasText = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: widget.controller,
+      decoration: InputDecoration(
+        hintText: 'Search',
+        prefixIcon: const Icon(Icons.search_outlined),
+        suffixIcon: Visibility(
+          visible: hasText,
+          child: InkWell(
+            onTap: () => setState(() {
+              if (widget.controller != null) {
+                widget.controller!.text = "";
+                hasText = false;
+              }
+              widget.onClearSearch();
+            }),
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.clear_outlined,
+                size: 16.0,
+              ),
+            ),
+          ),
+        ),
+      ),
+      onEditingComplete: () => widget.onSearch(),
+      onChanged: (value) => setState(() {
+        hasText = value.isNotEmpty;
+      }),
+    );
+  }
+}
