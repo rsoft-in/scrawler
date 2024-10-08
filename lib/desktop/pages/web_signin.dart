950d51fc29d9db2e9ce0f26422d6eee5094120e4
Rajesh Menon <rajeshmenon@rennovationsoftware.com>
2024-10-08 16:06:25 +0530

    signin to api service

--- lib/desktop/pages/web_signin.dart
+++ lib/desktop/pages/web_signin.dart
@@ -1,6 +1,15 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 import 'package:material_symbols_icons/symbols.dart';
+import 'package:scrawler/desktop/desktop_app.dart';
 import 'package:scrawler/helpers/constants.dart';
+import 'package:scrawler/models/users_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../helpers/globals.dart' as globals;
 
 class WebSignIn extends StatefulWidget {
   const WebSignIn({super.key});
@@ -10,8 +19,92 @@ class WebSignIn extends StatefulWidget {
 }
 
 class _WebSignInState extends State<WebSignIn> {
+  late SharedPreferences preferences;
   TextEditingController usernameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  List<User> users = [];
+
+  Future<void> setAPIServer() async {
+    try {
+      String server = await rootBundle.loadString('res/apiserver');
+      globals.apiServer = server;
+      getPreferences();
+    } catch (e) {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('$e'),
+            duration: const Duration(seconds: 3),
+          ),
+        );
+      }
+    }
+  }
+
+  void getPreferences() async {
+    preferences = await SharedPreferences.getInstance();
+    final appSignedIn = preferences.getBool("scrawler_signed_in") ?? false;
+    if (appSignedIn && mounted) {
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => const DesktopApp()),
+          (Route<dynamic> route) => false);
+    }
+  }
+
+  Future<void> signIn() async {
+    try {
+      var response = await http.Client().post(
+          Uri.parse("${globals.apiServer}/signin"),
+          headers: {'Content-Type': 'application/json'},
+          body: json.encode({
+            'username': usernameController.text,
+            'password': passwordController.text
+          }));
+      if (response.statusCode == 200) {
+        final parsed = json.decode(response.body);
+        users = parsed.map<User>((json) => User.fromJson(json)).toList();
+        if (mounted) {
+          if (users.isEmpty) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Invalid Username or Password!'),
+                duration: Duration(seconds: 3),
+              ),
+            );
+          } else {
+            setState(() {
+              globals.user = users[0];
+              preferences.setBool('scrawler_signed_in', true);
+              preferences.setString('user_id', globals.user!.userId);
+              preferences.setString('user_name', globals.user!.userName);
+              preferences.setString('user_email', globals.user!.userEmail);
+              preferences.setBool('user_enabled', globals.user!.userEnabled);
+            });
+            Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (context) => const DesktopApp()),
+                (Route<dynamic> route) => false);
+          }
+        }
+      }
+    } on Exception catch (e) {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('$e'),
+            duration: const Duration(seconds: 3),
+          ),
+        );
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setAPIServer();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +126,7 @@ class _WebSignInState extends State<WebSignIn> {
               TextFormField(
                 controller: usernameController,
                 decoration: const InputDecoration(
-                  hintText: 'Username',
+                  hintText: 'Email',
                   prefixIcon: Icon(Symbols.person),
                 ),
               ),
@@ -48,7 +141,7 @@ class _WebSignInState extends State<WebSignIn> {
               ),
               kVSpace,
               FilledButton(
-                onPressed: () {},
+                onPressed: () => signIn(),
                 child: const Text('Sign-In'),
               ),
               kVSpace,
