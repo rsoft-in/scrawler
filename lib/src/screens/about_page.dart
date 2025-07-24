961e1eae04c0758642528e6ca0b825ddd3c2d8ea
Rajesh Menon <rajeshmenon@rennovationsoftware.com>
2025-07-24 10:32:52 +0530

    about page changes

--- lib/src/screens/about_page.dart
+++ lib/src/screens/about_page.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:forui/forui.dart';
 
 class AboutPage extends StatefulWidget {
   const AboutPage({super.key});
@@ -11,11 +12,17 @@ class AboutPage extends StatefulWidget {
 class _AboutPageState extends State<AboutPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('about'.tr()),
+    return FScaffold(
+      header: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: FHeader.nested(
+          prefixes: [
+            FHeaderAction.back(onPress: () => Navigator.pop(context)),
+          ],
+          title: Text('about'.tr()),
+        ),
       ),
-      body: SingleChildScrollView(
+      child: SingleChildScrollView(
         child: Column(
           children: [],
         ),
